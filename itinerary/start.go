2d5505ac5d08b62f734ac7eea116ceb9f5be9020
adelinachachanidze <adelina.chachanidze@gmail.com>
2025-01-16 15:43:34 +0200

    added scenning and test input

--- itinerary/start.go
+++ itinerary/start.go
@@ -2,8 +2,24 @@ package itinerary
 
 import (
 	"fmt"
+	"os"
+	"bufio"
 )
 
 func Starting() {
-    fmt.Print( "This is the start of the itinerary!")
+	// Step 1: Open the file
+    file, err := os.Open("itinerary/testing.txt") // Open the file named "testing", and showing the path to it
+    if err != nil {
+        fmt.Println("Error opening file:", err)
+        return
+    }
+    defer file.Close()
+
+	 // Step 2: Read the file line by line and print each line
+	 scanner := bufio.NewScanner(file)
+    
+	 //Loop through the file line by line
+	 for scanner.Scan() {
+		 fmt.Println(scanner.Text()) // Print the current line
+	 }
 }
