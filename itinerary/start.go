0250ef99887290cb5b10f713f7377499d456fa94
adelinachachanidze <adelina.chachanidze@gmail.com>
2025-01-17 13:00:28 +0200

    added airport check and output create funcs

--- itinerary/start.go
+++ itinerary/start.go
@@ -7,30 +7,65 @@ import (
 )
 
 func Starting() {
-	// Step 1: Open the file
-	file, err := os.Open("itinerary/testing.txt") // Open the file named "testing", and showing the path to it
+	// Open the file named "testing", we need to write a path to it
+	// file -- the file handle if successful
+	// err -- any error that occurred during the operation (or nil if successful)
+	file, err := os.Open("itinerary/testing.txt")
 
+	//Error handling
 	if err != nil {
 		if os.IsNotExist(err) {
-			fmt.Println("File not found")
+			fmt.Println("Input file not found")
 		} else {
-			fmt.Println("Error opening file:", err)
+			fmt.Println("Error opening input file:", err)
 		}
 		return
 	}
 
 	defer file.Close()
 
-	// Step 2: Read the file line by line and print each line
-	scanner := bufio.NewScanner(file)
+	//Read the file line by line and print each line
+	inputScanner := bufio.NewScanner(file)
 
-	if !scanner.Scan() {
-		fmt.Println("The file is empty")
+	//Check if the file is empty
+	if !inputScanner.Scan() {
+		fmt.Println("The input file is empty")
 		return
 	}
 
 	//Loop through the file line by line
-	for scanner.Scan() {
-		fmt.Println(scanner.Text()) // Print the current line
+	for inputScanner.Scan() {
+		fmt.Println(inputScanner.Text())
+		// Returns the current line of text that the scanner has read as a string
+		// The text is returned as a string
+		// Part of the bufio package
 	}
+}
+
+func airpotsCheck() {
+	file, err := os.Open("itineraryairport-lookup.csv")
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("Airport lookup file not found")
+			return
+		}
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	fmt.Println("Airport lookup file found")
+
+	airportsScanner := bufio.NewScanner(file)
+
+	if !airportsScanner.Scan() {
+		fmt.Println("Airport lookup is empty")
+		return
+	}
+
+	defer file.Close()
+}
+
+func outputCreate() {
+	os.Create("output.txt")
 }
