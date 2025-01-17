eed5d33d7ba4ef9d4dd2fdd797dc1c9b8bf266f5
adelinachachanidze <adelina.chachanidze@gmail.com>
2025-01-17 11:32:39 +0200

    Finds and reads files, shows errors if not found, empty or errors opening

--- itinerary/start.go
+++ itinerary/start.go
@@ -1,25 +1,36 @@
 package itinerary
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
 )
 
 func Starting() {
 	// Step 1: Open the file
-    file, err := os.Open("itinerary/testing.txt") // Open the file named "testing", and showing the path to it
-    if err != nil {
-        fmt.Println("Error opening file:", err)
-        return
-    }
-    defer file.Close()
+	file, err := os.Open("itinerary/testing.txt") // Open the file named "testing", and showing the path to it
 
-	 // Step 2: Read the file line by line and print each line
-	 scanner := bufio.NewScanner(file)
-    
-	 //Loop through the file line by line
-	 for scanner.Scan() {
-		 fmt.Println(scanner.Text()) // Print the current line
-	 }
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("File not found")
+		} else {
+			fmt.Println("Error opening file:", err)
+		}
+		return
+	}
+
+	defer file.Close()
+
+	// Step 2: Read the file line by line and print each line
+	scanner := bufio.NewScanner(file)
+
+	if !scanner.Scan() {
+		fmt.Println("The file is empty")
+		return
+	}
+
+	//Loop through the file line by line
+	for scanner.Scan() {
+		fmt.Println(scanner.Text()) // Print the current line
+	}
 }
